9a0b613bce80f1c2641a5707cabcabe2b9f87589
Sergio Lombana <sergiolombana101@gmail.com>
2026-08-05 22:32:09 +0000

    fix in.euclio.io canary address

--- app/dashboard/incidents/[id]/page.tsx
+++ app/dashboard/incidents/[id]/page.tsx
@@ -1,4 +1,4 @@
-import { auth } from "@clerk/nextjs/server";
+caimport { auth } from "@clerk/nextjs/server";
 import { notFound, redirect } from "next/navigation";
 import Link from "next/link";
 import { getOrCreateAccountForCurrentUser } from "@/lib/account";
